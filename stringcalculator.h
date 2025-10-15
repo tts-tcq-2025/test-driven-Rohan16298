f7180b0537d02043268024a9d8a8c094265380ed
Rohan16298 <urk3hyd@bosch.com>
2025-10-15 09:43:27 +0530

    Update stringcalculator.h

--- stringcalculator.h
+++ stringcalculator.h
@@ -12,7 +12,10 @@ public:
 private:
     bool HasCustomDelimiter(const std::string &input) const;
     void ParseCustomDelimiter(const std::string &input, std::vector<std::string> &delimiters, std::string &numbersPart) const;
+    std::vector<std::string> ExtractDelimiters(const std::string &delimiterPart) const;
+    std::string ReplaceDelimitersWithComma(std::string temp, const std::vector<std::string> &delimiters) const;
     std::vector<int> SplitAndConvert(const std::string &numbersPart, const std::vector<std::string> &delimiters) const;
+    std::vector<int> GetNegatives(const std::vector<int> &numbers) const;
     void CheckNegatives(const std::vector<int> &numbers) const;
     int SumNumbers(const std::vector<int> &numbers) const;
 };
